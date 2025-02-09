3b1689bc2da69d46ff5a274b9191a2cc9e92558a
Sebastian Carballo <sebastiancarballoes@gmail.com>
2025-02-09 01:10:53 -0300

    feat: implement navigation links in Menu component
    - Replaced anchor tags with React Router's Link components for navigation
    - Improved routing efficiency and SPA behavior

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // 👈 Import Link
 
 function Menu() {
   return (
-    <nav>Menu</nav>
+    <nav>
+      <ul>
+{/*         <li>
+          <a href="/#/">Home</a> 
+        </li>
+        <li>
+          <a href="/#/blog">Blog</a> 
+        </li>
+        <li>
+          <a href="/#/profile">Profile</a> 
+        </li> 
+*/}
+        <li>
+          {/*  Compare to <a> tags:  href="" becomes to="" */}
+          <Link to="/">Home</Link> {/* Navigate to the Home route */}
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link> {/* Navigate to the Blog route */}
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link> {/* Navigate to the Profile route */}
+        </li>
+
+      </ul>
+    </nav>
   );
 }
 
