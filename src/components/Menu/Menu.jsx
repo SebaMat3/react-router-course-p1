2b1d2ea14e2cc98997f8910ee207ce55b2fbb2ca
Sebastian Carballo <sebastiancarballoes@gmail.com>
2025-02-10 10:57:11 -0300

    feat: NavLink example, using isActive prop

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // 👈 Import Link
+//import React from 'react';
+import { NavLink } from 'react-router-dom'; // 👈 Import NavLink
+
 
 function Menu() {
   return (
     <nav>
       <ul>
-{/*         <li>
-          <a href="/#/">Home</a> 
+        <li>
+{/*       <NavLink
+            to="/"
+            className={() => ''} // Example: Dynamic class (can be a function)
+            style={() => ({ background: '#fff' })} // Example: Dynamic style (can be a function)
+          >
+            Home
+          </NavLink> */}
+          <NavLink
+            to="/"
+            end // 👈  Important:  Match path exactly
+            style={({ isActive }) => ({ // Style function receiving isActive
+              color: isActive ? 'green' : 'red', // Green if active, red if not
+              fontWeight: isActive ? 'bold' : 'normal', // Bold if active
+            })}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <a href="/#/blog">Blog</a> 
+          <NavLink to="/blog">Blog</NavLink>
         </li>
         <li>
-          <a href="/#/profile">Profile</a> 
-        </li> 
-*/}
-        <li>
-          {/*  Compare to <a> tags:  href="" becomes to="" */}
-          <Link to="/">Home</Link> {/* Navigate to the Home route */}
+          <NavLink to="/profile">Profile</NavLink>
         </li>
-        <li>
-          <Link to="/blog">Blog</Link> {/* Navigate to the Blog route */}
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link> {/* Navigate to the Profile route */}
-        </li>
-
       </ul>
     </nav>
   );
