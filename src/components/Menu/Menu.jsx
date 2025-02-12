49a41200d928a81e1c2739a07c7593e9dc5b74c8
Sebastian Carballo <sebastiancarballoes@gmail.com>
2025-02-12 16:42:25 -0300

    feat: implement basic protected route access control
    - Add route protection based on authentication state
    - Restrict access to sensitive pages for unauthenticated users
    - Implement authentication checks in route components
    - Add redirection for unauthorized access attempts

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,30 +1,39 @@
-//import React from 'react';
+//src/components/Menu/Menu.jsx
+
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../auth/auth';
 
 const routes = []; 
-routes.push({ to: '/', text: 'Home' });
-routes.push({ to: '/blog', text: 'Blog' });
-routes.push({ to: '/profile', text: 'Profile' });
-routes.push({ to: '/login', text: 'Login' });  // 👈 Added Login
-routes.push({ to: '/logout', text: 'Logout' }); // 👈 Added Logout
+routes.push({ to: '/', text: 'Home', private: false });
+routes.push({ to: '/blog', text: 'Blog', private: false });
+routes.push({ to: '/profile', text: 'Profile', private: true });
+routes.push({ to: '/login', text: 'Login', private: false, publicOnly:true });  
+routes.push({ to: '/logout', text: 'Logout', private: true }); 
 
 function Menu() {
+  const auth = useAuth();
+
   return (
     <nav>
       <ul>
-        {routes.map(route => (
-          <li key={route.to}> {/* Add a key for React list rendering */}
-            <NavLink
-              to={route.to}
-              end // Use 'end' prop for exact matching
-              style={({ isActive }) => ({
-                color: isActive ? 'green' : 'red',
-              })}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map(route => {
+          if (route.publicOnly && auth.user) return null;
+          if (route.private && !auth.user) return null;
+
+          return (
+            <li key={route.to}> 
+              <NavLink
+                to={route.to}
+                end // Use 'end' prop for exact matching
+                style={({ isActive }) => ({
+                  color: isActive ? 'green' : 'red',
+                })}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          )    
+        })}
       </ul>
     </nav>
   );
